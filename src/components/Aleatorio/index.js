47dafc2be55196a5fa30284a05bfd0b457df0b2c
Marcelo Santos <marcelo.santos.dev@gmail.com>
2020-08-22 02:03:02 -0300

    Aleatory Number Challenge

--- src/components/Aleatorio/index.js
+++ src/components/Aleatorio/index.js
@@ -2,13 +2,13 @@ import React from 'react';
 import { View, Text } from 'react-native';
 import GlobalStyles from '../GlobalStyles'
 
-export default (props) => {
-    const randomNumber = Math.floor(Math.random() * (props.max - props.min) + props.min);
+export default ({ min, max }) => {
+    const randomNumber = Math.floor(Math.random() * (max - min) + min);
   return (
       <>
         <View>
             <Text style={GlobalStyles.txtH3}>
-                O número aleatório entre {props.min} e {props.max} é: {randomNumber} 
+                O número aleatório entre {min} e {max} é: {randomNumber} 
             </Text>
         </View>
       </>
